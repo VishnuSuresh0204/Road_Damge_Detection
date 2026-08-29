6291ec8bc4a812ba79e206e5d126ec22aa41b289
Vishnu Suresh <vishnu.suresh0204@gmail.com>
2026-08-29 12:36:52 +0530

    Update project files

--- r_damage/myapp/models.py
+++ r_damage/myapp/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Login(AbstractUser):
+    USER_TYPES = [
+        ('Admin', 'Admin'),
+        ('User', 'User'),
+    ]
+
+    usertype = models.CharField(
+        max_length=50,
+        choices=USER_TYPES,
+        default='User'
+    )
+    viewpassword = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.username
+
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15, blank=True)
+    address = models.TextField(blank=True)
+
+    profile_pic = models.ImageField(
+        upload_to='profiles/',
+        null=True,
+        blank=True
+    )
+
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
--- r_damage/r_damage/settings.py
+++ r_damage/r_damage/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'r_damage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL= "myapp.Login"
